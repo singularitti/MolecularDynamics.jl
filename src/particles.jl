4c9353e6fd4faf45cc0849956e1170df8a38eebf
singularitti <singularitti@outlook.com>
2022-09-28 00:05:35 -0400

    Add iteration interface `EachParticle` to `SimulationCell` in src/particles.jl

--- src/particles.jl
+++ src/particles.jl
@@ -42,3 +42,24 @@ end
 boxsize(cell::SimulationCell) = length(cell.particles) / cell.density
 
 boxlength(cell::SimulationCell) = cbrt(boxsize(cell))
+
+struct EachParticle
+    cell::SimulationCell
+end
+
+eachparticle(cell::SimulationCell) = EachParticle(cell)
+
+# Similar to https://github.com/JuliaCollections/IterTools.jl/blob/0ecaa88/src/IterTools.jl#L1028-L1032
+function Base.iterate(iter::EachParticle, state=1)
+    if state > length(iter)
+        return nothing
+    else
+        return iter.cell.particles[state], state + 1
+    end
+end
+
+Base.eltype(::EachParticle) = Particle
+
+Base.length(iter::EachParticle) = length(iter.cell.particles)
+
+Base.IteratorSize(::Type{<:EachParticle}) = Base.HasLength()
