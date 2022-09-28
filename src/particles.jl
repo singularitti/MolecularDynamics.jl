3ba1405cf3995c296b63df77b7d5ac14852551eb
singularitti <singularitti@outlook.com>
2022-09-27 23:50:59 -0400

    Update src/particles.jl

--- src/particles.jl
+++ src/particles.jl
@@ -1,13 +1,44 @@
-using StaticArrays: SVector, MVector
+using StaticArrays: MVector
+
+export Particle, SimulationCell
+export distance
 
 mutable struct Particle
-    r::MVector{3,Float64}
-    v::MVector{3,Float64}
-    Particle(r, v) = new(r, v)
+    position::MVector{3,Float64}
+    velocity::MVector{3,Float64}
+    Particle(position, velocity) = new(position, velocity)
+    Particle(position) = new(position)
     Particle() = new()  # Incomplete initialization
 end
 
-function distance(particle::Particle, particle′::Particle)
-    return sqrt(sum(abs2, particle.r - particle′.r))
+struct SimulationCell
+    particles::Vector{Particle}
+    density::Float64
 end
 
+function distance(particle::Particle, particle′::Particle)
+    return sqrt(sum(abs2, particle.position - particle′.position))
+end
+
+function apply_periodic_bc(x, L)
+    return if x > L / 2
+        x + L
+    elseif x < -L / 2
+        x - L
+    else  # abs(x) < L / 2
+        x
+    end
+end
+
+function list_interacting_particles(cell::SimulationCell, i)
+    return map(filter(!=(i), eachindex(cell.particles))) do j
+        particleᵢ, particleⱼ = cell.particles[[i, j]]
+        Δ𝐫 = particleᵢ.position - particleⱼ.position
+        coordinates = map(Base.Fix1(apply_periodic_bc, Δ𝐫), boxlength(cell))
+        Particle(coordinates)
+    end
+end
+
+boxsize(cell::SimulationCell) = length(cell.particles) / cell.density
+
+boxlength(cell::SimulationCell) = cbrt(boxsize(cell))
