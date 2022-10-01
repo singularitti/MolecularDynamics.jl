2c7244f8f29016bde0bd8a4cfc7a36e6786df211
singularitti <singularitti@outlook.com>
2022-10-01 00:02:06 -0400

    Update plotting.py

--- scripts/plotting.py
+++ scripts/plotting.py
@@ -28,13 +28,13 @@ def readpositions(filename, axes):
         pass
 
 
-def plot(files, ax=plt.axes(projection="3d")):
+def plot(files, ax=plt.axes(projection="3d"), **kwargs):
     fig = plt.figure(figsize=(16, 10))
     for file in files:
         x = readpositions(file, "x")
         y = readpositions(file, "y")
         z = readpositions(file, "z")
-        ax.scatter3D(x, y, z, label=file)
+        ax.scatter(x, y, z, label=file, s=5, **kwargs)
     # See https://stackoverflow.com/a/63625222/3260253
     ax.set_box_aspect([1, 1, 1])
     ax.legend(loc="best")
@@ -46,13 +46,13 @@ def plot(files, ax=plt.axes(projection="3d")):
 
 
 if __name__ == "__main__":
-    data = readpositions("../p.json", "all")
-    arr = ["../p.json"]
+    data = readpositions("all points in the center cell", "all")
+    arr = ["all points in the center cell"]
     # r = range(10, 21, 10)
-    r = (29, 49)
+    r = (499, 999)
     for i in r:
-        arr.append("../" + str(i+1) + ".json")
-    fig, ax = plot(arr)
+        arr.append("neighbors of the " + str(i+1) + "th point")
+    fig, ax = plot(arr, zorder=0)
     for i in r:
-        ax.scatter(*data[i], label=str(i+1), s=40)
+        ax.scatter(*data[i], label="the " + str(i+1) + "th point", s=60)
     ax.legend(loc="best")
