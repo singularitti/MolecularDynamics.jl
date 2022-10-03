87d2c0bc37de7eaf559846354b1a47da51974cbb
singularitti <singularitti@outlook.com>
2022-10-03 03:17:12 -0400

    Add `show` method for `TimeStepTracker`

--- src/integrators.jl
+++ src/integrators.jl
@@ -57,3 +57,20 @@ function positions(tracker::TimeStepTracker)
         particle.position
     end
 end
+
+function Base.show(io::IO, tracker::TimeStepTracker)
+    if get(io, :compact, false) || get(io, :typeinfo, nothing) == typeof(tracker)
+        Base.show_default(IOContext(io, :limit => true), tracker)  # From https://github.com/mauro3/Parameters.jl/blob/ecbf8df/src/Parameters.jl#L556
+    else
+        println(io, summary(tracker))
+        println(io, " time step: ", tracker.Δt)
+        print(
+            io,
+            " total ",
+            size(tracker.steps, 2),
+            " steps for ",
+            size(tracker.steps, 1),
+            " particles",
+        )
+    end
+end
