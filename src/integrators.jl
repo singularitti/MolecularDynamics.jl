dce615d868caae0d259b2604a2f8b7bcfc7b68aa
singularitti <singularitti@outlook.com>
2022-10-03 01:30:10 -0400

    Add field `Δt` to `TimeStepTracker` & rename it to `TimeStepTracker`

--- src/integrators.jl
+++ src/integrators.jl
@@ -1,13 +1,14 @@
 using ElasticArrays: ElasticMatrix
 using ProgressMeter: @showprogress
 
-export VelocityVerlet
+export VelocityVerlet, TimeStepTracker
 export take_one_step!, take_n_steps!, velocities, positions
 
 abstract type Integrator end
 struct VelocityVerlet <: Integrator end
 
-struct StepTracker
+struct TimeStepTracker
+    Δt::Float64
     steps::ElasticMatrix{Particle}
 end
 
@@ -35,23 +36,23 @@ function take_n_steps!(cell::Cell, n, Δt, ::VelocityVerlet)
         take_one_step!(cell, Δt, VelocityVerlet())
         data[:, i] = deepcopy(cell.particles)
     end
-    return StepTracker(data)
+    return TimeStepTracker(Δt, data)
 end
-function take_n_steps!(tracker::StepTracker, cell::Cell, n, Δt, ::VelocityVerlet)
+function take_n_steps!(tracker::TimeStepTracker, cell::Cell, n, ::VelocityVerlet)
     @showprogress for _ in 1:n
-        take_one_step!(cell, Δt, VelocityVerlet())
+        take_one_step!(cell, tracker.Δt, VelocityVerlet())
         append!(tracker.steps, deepcopy(cell.particles))
     end
     return tracker
 end
 
-function velocities(tracker::StepTracker)
+function velocities(tracker::TimeStepTracker)
     return map(tracker.steps) do particle
         particle.velocity
     end
 end
 
-function positions(tracker::StepTracker)
+function positions(tracker::TimeStepTracker)
     return map(tracker.steps) do particle
         particle.position
     end
