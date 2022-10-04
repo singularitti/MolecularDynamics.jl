d773873b34884b81cfb527655c8f6c63910feef9
singularitti <singularitti@outlook.com>
2022-10-04 07:39:33 -0400

    Rename & rewrite `ObservableLogger` to `Logger`

--- src/loggers.jl
+++ src/loggers.jl
@@ -1,15 +1,13 @@
-using ElasticArrays: ElasticMatrix
+using ElasticArrays: ElasticVector
+using StaticArrays: SVector
 
-export ObservableLogger
+export Logger
 export extract_history, nsteps
 
-struct ObservableLogger{T}
+mutable struct Logger
     Δt::Float64
-    history::ElasticMatrix{T}
+    history::ElasticVector{SVector{3,Particle}}
 end
-
-const VelocityLogger = ObservableLogger{Velocity}
-const CoordinatesLogger = ObservableLogger{Coordinates}
 
 function extract_history(logger::VelocityLogger)
     return map(logger.history) do particle
@@ -22,9 +20,9 @@ function extract_history(logger::CoordinatesLogger)
     end
 end
 
-nsteps(logger::ObservableLogger) = size(logger.history, 2)
+nsteps(logger::Logger) = size(logger.history, 2)
 
-function Base.show(io::IO, logger::ObservableLogger)
+function Base.show(io::IO, logger::Logger)
     if get(io, :compact, false) || get(io, :typeinfo, nothing) == typeof(logger)
         Base.show_default(IOContext(io, :limit => true), logger)  # From https://github.com/mauro3/Parameters.jl/blob/ecbf8df/src/Parameters.jl#L556
     else
