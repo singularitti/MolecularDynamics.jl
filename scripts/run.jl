8e1a0b28dd5d8e3123eae68744410bfe0a59892c
singularitti <singularitti@outlook.com>
2022-10-05 04:51:05 -0400

    Rename args in scripts/run.jl

--- scripts/run.jl
+++ scripts/run.jl
@@ -2,27 +2,32 @@ using LinearAlgebra
 using MolecularDynamics
 using Plots
 
-p = [Particle(rand(3), rand(3)) for _ in 1:1000];
-b = CubicBox(length(p), 0.75)
-init!(p, b, Even(), Uniform(zeros(Velocity)));
-lg = Logger(length(p))
+particles = [Particle(rand(3), rand(3)) for _ in 1:1000];
+box = CubicBox(length(particles), 0.75)
+init!(particles, box, Even(), Uniform(zeros(Velocity)));
+logger = Logger(length(particles))
 Δt = 0.032
 
-take_n_steps!(lg, p, b, 5000, Δt, VelocityVerlet())
+take_n_steps!(logger, particles, box, 5000, Δt, VelocityVerlet())
 # sort(norm.(extract(Velocity, lg, length(p))))
 
-u = map(lg.history) do step
+U = map(logger.history) do step
     potential_energy(step.snapshot)
 end
-k = map(lg.history) do step
+T = map(logger.history) do step
     kinetic_energy(step.snapshot)
 end
-e = u .+ k
+E = U .+ T
 
-fig = plot(range(0; step=Δt, length=length(lg.history)), k; label="kinetic energy")
-plot!(fig, range(0; step=Δt, length=length(lg.history)), u; label="potential energy")
-plot!(fig, range(0; step=Δt, length=length(lg.history)), e; label="total energy")
+fig = plot(
+    range(0; step=Δt, length=length(logger.history)),
+    T;
+    label="kinetic energy",
+    framestyle=:box,
+)
+plot!(fig, range(0; step=Δt, length=length(logger.history)), U; label="potential energy")
+plot!(fig, range(0; step=Δt, length=length(logger.history)), E; label="total energy")
+xlim!((0, simulation_time(logger)))
 xlabel!("time")
 ylabel!("energy")
-plot!(; framestyle=:box)
 savefig("e-t.pdf")
