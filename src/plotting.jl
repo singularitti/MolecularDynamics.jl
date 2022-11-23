660c1011349150c5f2bcacb135a679b729931c8e
singularitti <singularitti@outlook.com>
2022-11-23 01:42:50 -0500

    Add type & method `CoordPlot` in src/plotting.jl using `@recipe`

--- src/plotting.jl
+++ src/plotting.jl
@@ -1,10 +1,23 @@
-using RecipesBase: RecipesBase, @recipe
+using RecipesBase: RecipesBase, @userplot, @recipe
 
-@recipe function f(particles::AbstractVector{Particle})
-    # Set a default value for an attribute with `-->`
-    xlabel --> "x"
-    ylabel --> "y"
-    zlabel --> "z"
+export coordplot
+
+@userplot CoordPlot
+@recipe function f(plot::CoordPlot)
+    # See http://juliaplots.org/RecipesBase.jl/stable/types/#User-Recipes-2
+    particles = plot.args[end]  # Extract `magnetization` from the args
+    seriestype --> :scatter3d
+    markersize --> 1
+    markerstrokecolor --> :auto
+    markerstrokewidth --> 0
+    xguide --> raw"$x / \sigma$"
+    yguide --> raw"$y / \sigma$"
+    zguide --> raw"$z / \sigma$"
+    guidefontsize --> 10
+    tickfontsize --> 8
+    legendfontsize --> 8
+    legend_foreground_color --> nothing
+    frame --> :box
     X, Y, Z = getcoordinates(particles)()
     return X, Y, Z
 end
