ff950978d6fef9c86e175cf181976edb1d192b67
singularitti <singularitti@outlook.com>
2022-12-01 13:37:59 -0500

    Merge two `take_n_steps!` methods into one
    since I often forget how to use it

--- src/integrators.jl
+++ src/integrators.jl
@@ -25,16 +25,11 @@ function take_n_steps!(particles, box::Box, n, Δt, ::VelocityVerlet)
     end
     return particles
 end
-function take_n_steps!(
-    logger::Logger{N}, particles, box::Box, n, Δt, ::VelocityVerlet
-) where {N}
+function take_n_steps!(logger::Logger{N}, box::Box, n, Δt, ::VelocityVerlet) where {N}
+    particles = logger.history[end].snapshot
     @showprogress for _ in 1:n
         take_one_step!(particles, box, Δt, VelocityVerlet())
         push!(logger.history, Step(Δt, SVector{N}(deepcopy(particles))))
     end
     return particles
-end
-function take_n_steps!(logger::Logger, box::Box, n, Δt, ::VelocityVerlet)
-    particles = logger.history[end].snapshot
-    return take_n_steps!(logger, particles, box, n, Δt, VelocityVerlet())
 end
