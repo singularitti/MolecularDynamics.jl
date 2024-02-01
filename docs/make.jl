cef9d6880bb2e6139538f8c3e51558b05d550cc9
singularitti <singularitti@outlook.com>
2024-01-31 19:32:35 -0500

    Update docs

--- docs/make.jl
+++ docs/make.jl
@@ -6,10 +6,8 @@ DocMeta.setdocmeta!(MolecularDynamics, :DocTestSetup, :(using MolecularDynamics)
 makedocs(;
     modules=[MolecularDynamics],
     authors="singularitti <singularitti@outlook.com> and contributors",
-    repo="https://github.com/singularitti/MolecularDynamics.jl/blob/{commit}{path}#{line}",
     sitename="MolecularDynamics.jl",
     format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://singularitti.github.io/MolecularDynamics.jl",
         edit_link="main",
         assets=String[],
