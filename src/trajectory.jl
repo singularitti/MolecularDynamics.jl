6dad495b0c81ac17383fac12747cd391d803ec9f
singularitti <singularitti@outlook.com>
2024-02-02 05:32:34 -0500

    Deprecate `extract`

--- src/trajectory.jl
+++ src/trajectory.jl
@@ -1,7 +1,6 @@
 using ElasticArrays: ElasticVector
 
-export Step, Trajectory
-export extract, simulation_time
+export Step, Trajectory, simulation_time
 
 struct Step{T,S}
     dt::T
@@ -12,27 +11,6 @@ struct Trajectory{S<:Step} <: AbstractVector{S}
     data::ElasticVector{S}
 end
 Trajectory(data::AbstractArray) = Trajectory(ElasticVector(data))
-
-function extract(::Type{T}, trajectory::Trajectory) where {T}
-    return map(trajectory.data) do step
-        map(step.snapshot) do particle
-            extract(T, particle)
-        end
-    end
-end
-function extract(::Type{T}, trajectory::Trajectory, m::Integer) where {T}
-    particles = trajectory.data[m].snapshot
-    return map(particles) do particle
-        extract(T, particle)
-    end
-end
-function extract(::Type{T}, trajectory::Trajectory, m::Integer, n::Integer) where {T}
-    particle = trajectory.data[m].snapshot[n]
-    return extract(T, particle)
-end
-extract(::Type{Velocity}, particle::Particle) = getfield(particle, :velocity)
-extract(::Type{Coordinates}, particle::Particle) = getfield(particle, :coordinates)
-extract(::Type{Particle}, particle::Particle) = particle
 
 simulation_time(trajectory::Trajectory) = cumsum(step.Δt for step in trajectory)
 
