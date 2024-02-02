64caffe8c42fb0eb3fd5c432653726b67edf4578
singularitti <singularitti@outlook.com>
2024-02-02 05:28:09 -0500

    Rename field of `VelocityRescaling`

--- src/thermostats.jl
+++ src/thermostats.jl
@@ -3,13 +3,13 @@ export thermostat!
 
 abstract type Thermostat end
 struct VelocityRescaling <: Thermostat
-    desired_temperature::Float64
+    target_temperature::Float64
 end
 
 function thermostat!(particles, thermostat::VelocityRescaling)
     t = temperature(particles)
     for particle in particles
-        particle.velocity *= sqrt(thermostat.desired_temperature / t)
+        particle.velocity *= sqrt(thermostat.target_temperature / t)
     end
     return particles
 end
