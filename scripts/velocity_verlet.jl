fc552b91eb8c847abf60318028bdd410bf81191a
singularitti <singularitti@outlook.com>
2024-02-03 04:27:19 -0500

    Fix scripts/velocity_verlet.jl

--- scripts/velocity_verlet.jl
+++ scripts/velocity_verlet.jl
@@ -1,4 +1,5 @@
 using MolecularDynamics
+using ProgressMeter: @showprogress
 using Unitful: k, @u_str
 using Plots
 
@@ -23,25 +24,31 @@ particles = [
         mass,
         zeros(Coordinates{typeof(1.0u"angstrom")}),
         zeros(Velocity{typeof(1.0u"angstrom/fs")}),
-    ) for _ in 1:864
+    ) for _ in 1:N
 ];
 cell = CubicCell(length(particles), 0.75u"angstrom^-3")
 init_coordinates!(particles, cell, Random());
 init_velocities!(particles, Constant(zeros(Velocity{typeof(1.0u"angstrom/s")})));
 Δt = 1e-18u"s"
 
-for _ in 1:50
+for _ in 1:55
     relax!(particles, cell, Δt, 1)
     println(potential_energy(particles))
 end
 
 integrator = VelocityVerlet()
-trajectory = integrate!(particles, cell, Δt, 1000, integrator);
+trajectory = integrate!(particles, cell, Δt, 1, integrator);
 
-while abs(temperature(particles, k) - target_t) / target_t >= 0.001
-    trajectory′ = integrate!(particles, cell, Δt, 500, integrator)
-    append!(trajectory, trajectory′)
-    thermostat!(particles, VelocityRescaling(target_t))
+@showprogress dt = 3 for round in 1:10
+    error = abs(temperature(particles, k) - target_T) / target_T
+    println("round: ", round, ", error: ", error)
+    if error <= 0.001
+        break
+    else
+        trajectory′ = integrate!(particles, cell, Δt, 500, integrator)
+        append!(trajectory, trajectory′)
+        thermostat!(particles, k, VelocityRescaling(target_T))
+    end
 end
 
 energyplot(trajectory)
@@ -62,4 +69,4 @@ function plothist(step)
     return current()
 end
 
-temperatureplot(trajectory)
+temperatureplot(trajectory, k)
